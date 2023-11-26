ea10374d3f72ba3c68d55f1ef731d021c5df1029
Jesus Gallardo <jjgallardior@gmail.com>
2023-11-26 11:09:32 -0400

    Image header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export function Header () {
       <header className='flex justify-between items-center py-5 z-20 bg-white dark:bg-gray-800 bg-opacity-80 dark:bg-opacity-80 backdrop-blur-lg'>
         <section className='flex items-center gap-4'>
           <picture className='bg-white ring-2 ring-offset-2 ring-gray-600 dark:ring-offset-gray-800 dark:ring-gray-500 rounded-full'>
-            <img src='/me.jpeg' alt='Jesus Gallardo' className='block object-cover object-top w-10 h-10 rounded-full' />
+            <img src='/me.webp' alt='Jesus Gallardo' loading='lazy' className='block object-cover object-top w-10 h-10 rounded-full' />
           </picture>
           <h3 className='font-semibold text-lg whitespace-nowrap text-slate-600 dark:text-slate-300'>
             Gallardo JA
